4becece89e26f13f04f78c7142f49f0c4130d004
Oyesola Ogundele <oyekunmi@yahoo.com>
2022-02-07 21:29:10 +0400

    bumping to v1.0.17

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ const env = process.env.APP_VARIANT;
 export default {
   name: getAppName(),
   slug: getSlug(),
-  version: "1.0.16",
+  version: "1.0.17",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   // jsEngine: "hermes",
@@ -28,7 +28,7 @@ export default {
   ],
   android: {
     package: getAppIdentifier(),
-    versionCode: 16,
+    versionCode: 17,
     googleServicesFile: "./google-services.json",
     adaptiveIcon: {
       foregroundImage: "./assets/images/icon.png",
