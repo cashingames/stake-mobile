f4730c10e63a66e6990c0055fe1916399e890f93
Oyesola Ogundele <oyekunmi@yahoo.com>
2022-02-12 15:10:17 +0400

    adding back "cleaning leaderboard"
    This reverts commit cefb05c151066e0935d36a79f743ab4ce5d050fc.

--- src/shared/GlobalTopLeaders.js
+++ src/shared/GlobalTopLeaders.js
@@ -9,26 +9,26 @@ import TopLeader from './TopLeader';
 function GlobalTopLeaders({ leaders }) {
 
     const topLeaders = leaders?.slice(0, 3) ?? null;
-    const firstLeader = topLeaders[0] ?? { first_name: "..." };
-    const secondLeader = topLeaders[1] ?? { first_name: "..." };
-    const thirdLeader = topLeaders[2] ?? { first_name: "..." };
+    const firstLeader = topLeaders[0] ?? { username: "..." };
+    const secondLeader = topLeaders[1] ?? { username: "..." };
+    const thirdLeader = topLeaders[2] ?? { username: "..." };
     return (
         <View style={styles.content}>
             {topLeaders.length > 0 ? <>
                 <TopLeader
                     podPosition={require('../../assets/images/position3.png')}
-                    name={`${thirdLeader.first_name} ${thirdLeader.last_name}`}
-                    point={`${formatNumber(thirdLeader.points)} pts`}
+                    name={`${thirdLeader.username}`}
+                    point={`${formatNumber(thirdLeader.points ? `${thirdLeader.points}` : 0)} pts`}
                     avatar={thirdLeader.avatar} />
                 <TopLeader
                     podPosition={require('../../assets/images/position1.png')}
-                    name={`${firstLeader.first_name} ${firstLeader.last_name}`}
-                    point={`${formatNumber(firstLeader.points)} pts`}
+                    name={`${firstLeader.username}`}
+                    point={`${formatNumber(firstLeader.points ? `${firstLeader.points}` : 0)} pts`}
                     avatar={firstLeader.avatar} />
                 <TopLeader
                     podPosition={require('../../assets/images/position2.png')}
-                    name={`${secondLeader.first_name} ${secondLeader.last_name}`}
-                    point={`${formatNumber(secondLeader.points)} pts`}
+                    name={`${secondLeader.username}`}
+                    point={`${formatNumber(secondLeader.points ? `${secondLeader.points}` : 0)} pts`}
                     avatar={secondLeader.avatar} />
             </> : <></>
             }
