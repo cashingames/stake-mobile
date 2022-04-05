67859252ca8fdcb3dcf2a635114aa6cf3e341cb7
arunajoy2602 <arunajoy2602@gmail.com>
2022-04-05 11:43:21 +0400

    clean up

--- src/features/Auth/LoginScreen.js
+++ src/features/Auth/LoginScreen.js
@@ -21,8 +21,8 @@ export default function LoginScreen({ navigation }) {
 
     const dispatch = useDispatch();
 
-    const [email, setEmail] = useState(isStaging ? 'arunajoy2602@gmail.com' : 'arunajoy2602@yahoo.com' );
-    const [password, setPassword] = useState(isStaging ? '12345678' : '12345678');
+    const [email, setEmail] = useState(isStaging ? 'arunajoy2602@gmail.com' : '' );
+    const [password, setPassword] = useState(isStaging ? '12345678' : '');
     const [canLogin, setCanLogin] = useState(false);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState('');
