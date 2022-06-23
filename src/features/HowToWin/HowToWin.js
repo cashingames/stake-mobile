cfa35fbc12551666d8186646f25efe74a6e8bbaa
arunajoy2602 <arunajoy2602@gmail.com>
2022-06-23 12:10:32 +0400

    more clean up

--- src/features/HowToWin/HowToWin.js
+++ src/features/HowToWin/HowToWin.js
@@ -1,16 +1,40 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Text, View, Image, ScrollView } from 'react-native';
-import { useFocusEffect } from '@react-navigation/native';
+import React from 'react';
+import { Text,  ScrollView } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
-import Constants from 'expo-constants';
+import normalize from '../../utils/normalize';
 
 
 const HowToWin = () => {
     return (
-        <View>
-            <Text>How to win</Text>
-        </View>
+        <ScrollView style={styles.container}>
+            <Text style={styles.question}>How can i win on Cashingames?</Text>
+            <Text style={styles.answer}>Winning on Cashingames is very easy. You choose the category you are most knowledgeable in, answer
+                the questions and you stand a chance to win. The more questions you answer,the more you increase your winnings.
+            </Text>
+        </ScrollView>
     )
 }
-export default HowToWin
+
+export default HowToWin;
+
+const styles = EStyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#F2F5FF',
+        paddingHorizontal: normalize(18),
+        paddingVertical: normalize(25),
+    },
+    question: {
+        fontFamily: 'graphik-medium',
+        fontSize: '0.9rem',
+        marginVertical: normalize(20),
+        color: '#000000',
+        lineHeight: '1.3rem'
+    },
+    answer: {
+        fontFamily: 'graphik-regular',
+        fontSize: '0.9rem',
+        color: '#151C2F',
+        lineHeight: '1.3rem'
+    }
+});
