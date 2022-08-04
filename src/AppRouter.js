2e73c44df9f2898ea3523f0dfe9837e94f6ca223
arunajoy2602 <arunajoy2602@gmail.com>
2022-08-04 14:41:17 +0400

    updates on google signin

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -57,7 +57,6 @@ import MyChallengesScoreScreen from './features/Games/MyChallengesScoreScreen.js
 import SignupVerifyEmailScreen from './features/Auth/SignupVerifyEmailScreen';
 import EmailVerifiedScreen from './features/Auth/EmailVerifiedScreen';
 import ChallengeNotPendingScreen from './features/Games/ChallengeNotPendingScreen';
-import WelcomeScreen from './features/Auth/WelcomeScreen';
 
 const AppStack = createNativeStackNavigator();
 
@@ -190,8 +189,6 @@ function AppRouter() {
 			<AppStack.Screen name="Answer" component={SupportAnswerScreen} options={{ title: 'Details' }} />
 			<AppStack.Screen name="EmailVerified" component={EmailVerifiedScreen} options={{ headerShown: false }} />
 			<AppStack.Screen name="ChallengeNotPending" component={ChallengeNotPendingScreen} options={{ headerShown: false }} />
-			<AppStack.Screen name="Welcome" component={WelcomeScreen} options={{ headerShown: false }} />
-
 			{/* <AppStack.Screen name="Tournament" component={TournamentScreen} options={{ title: 'Tournament' }} /> */}
 		</AppStack.Navigator >
 	)
