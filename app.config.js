bedf70748df11318264a499f25aeef48eef34296
arunajoy2602 <arunajoy2602@gmail.com>
2022-08-04 23:02:31 +0400

    updating version and runtime number

--- app.config.js
+++ app.config.js
@@ -1,11 +1,11 @@
 const env = process.env.APP_VARIANT;
-const version = "1.0.45"; //Update for every build and publish
+const version = "1.0.44"; //Update for every build and publish
 
 export default {
   name: getAppName(),
   slug: getSlug(),
   version: version, //For every publish and build update the version. Update for every publish and new build
-  runtimeVersion: "1.42", //All apps using the same runtime will get the published updates. Generally update for every new build
+  runtimeVersion: "1.44", //All apps using the same runtime will get the published updates. Generally update for every new build
   orientation: "portrait",
   icon: "./assets/images/adaptive-icon2.png",
   // jsEngine: "hermes",
