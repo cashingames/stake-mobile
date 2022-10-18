6b63751825ccbb3b8c37574b53f491bdfb17bffe
arunajoy2602 <arunajoy2602@gmail.com>
2022-10-18 18:03:48 +0400

    ux updates

--- src/features/Auth/LoginScreen.js
+++ src/features/Auth/LoginScreen.js
@@ -163,7 +163,7 @@ const RenderCreateAccount = () => {
             </View>
             <Text style={styles.signInText}>or</Text>
             <View style={styles.google}>
-                <SocialSignUp googleText="SIGN IN" />
+                <SocialSignUp googleText="Sign in" />
             </View>
         </View>
     )
