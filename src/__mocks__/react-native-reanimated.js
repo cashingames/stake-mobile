9f751d534a8e99c9ff7f00b8b252404772b22593
Oyesola Ogundele <oyekunmi@gmail.com>
2022-12-28 07:52:18 +0400

    refractor testing

--- src/__mocks__/react-native-reanimated.js
+++ src/__mocks__/react-native-reanimated.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler/jestSetup';
+import Reanimated from 'react-native-reanimated/mock';
 
 jest.mock('react-native-reanimated', () => {
-    const Reanimated = require('react-native-reanimated/mock');
 
     // The mock for `call` immediately calls the callback which is incorrect
     // So we override it with a no-op
