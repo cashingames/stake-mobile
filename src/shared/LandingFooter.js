76f54f227f27643ba09169e1c5d7d0736abb1084
Aruna Joy <arunajoy2602@gmail.com>
2023-01-17 19:33:18 +0400

    adding contact us link to landing page

--- src/shared/LandingFooter.js
+++ src/shared/LandingFooter.js
@@ -7,85 +7,93 @@ import EStyleSheet from 'react-native-extended-stylesheet'
 const LandingFooter = () => {
     const navigation = useNavigation()
 
-  return (
-    <View style={styles.landingFooter}>
-      <View style={styles.footerLogo}>
-        <Image source={require('../../assets/images/logo-small.png')} />
-      </View>
-      <View style={styles.footerLinkContainer}>
-        <Pressable onPress={() => navigation.navigate('Terms')}> 
-            <Text style={styles.linkLabel}>Terms and Conditions</Text>
-        </Pressable>
-        <Pressable onPress={() => navigation.navigate('Support')}> 
-            <Text style={styles.linkLabel}>FAQ</Text>
-        </Pressable>
-        <Pressable onPress={() => navigation.navigate('Privacy')}>
-            <Text style={styles.linkLabel}>Privacy and Policy</Text>
-        </Pressable>
-      </View>
-      <View style={styles.socialMediaLinks}>
-        <View>
-            <Text style={styles.copyrightTexts}>Copyright Cashingames.</Text>
-            <Text style={styles.copyrightTexts}>All Rights Reserved.</Text>
+    return (
+        <View style={styles.landingFooter}>
+            <View style={styles.footerLogo}>
+                <Image source={require('../../assets/images/logo-small.png')} />
+            </View>
+            <View style={styles.footerLinkContainer}>
+                <Pressable onPress={() => navigation.navigate('Terms')}>
+                    <Text style={styles.linkLabel}>Terms and Conditions</Text>
+                </Pressable>
+                <Pressable onPress={() => navigation.navigate('Support')}>
+                    <Text style={styles.linkLabel}>FAQ</Text>
+                </Pressable>
+                <Pressable onPress={() => navigation.navigate('Privacy')}>
+                    <Text style={styles.linkLabel}>Privacy and Policy</Text>
+                </Pressable>
+            </View>
+            <View style={styles.socialMediaLinks}>
+                <View>
+                    <Text style={styles.copyrightTexts}>Copyright Cashingames.</Text>
+                    <Text style={styles.copyrightTexts}>All Rights Reserved.</Text>
+                </View>
+                <View style={styles.socialMediaIcons}>
+                    <FontAwesome5 name="twitter" size={24} color="#fff" style={styles.icon}
+                        onPress={() => Linking.openURL('https://www.twitter.com/cashingamesng')} />
+                    <FontAwesome5 name="instagram" size={24} color="#fff" style={styles.icon}
+                        onPress={() => Linking.openURL('https://www.instagram.com/cashingames')} />
+                    <FontAwesome5 name="facebook" size={24} color="#fff"
+                        style={styles.icon}
+                        onPress={() => Linking.openURL('https://www.facebook.com/cashingames')} />
+                </View>
+            </View>
+            <Text style={styles.contactUs} onPress={() => navigation.navigate('AuthContact')}>Need help ? Contact us</Text>
         </View>
-        <View style={styles.socialMediaIcons}>
-        <FontAwesome5 name="twitter" size={24} color="#fff"  style={styles.icon}
-        onPress={() => Linking.openURL('https://www.twitter.com/cashingamesng')}/>
-        <FontAwesome5 name="instagram" size={24} color="#fff" style={styles.icon}
-        onPress={() => Linking.openURL('https://www.instagram.com/cashingames')} />
-        <FontAwesome5 name="facebook" size={24} color="#fff" 
-        style={styles.icon}
-        onPress={() => Linking.openURL('https://www.facebook.com/cashingames')}/>
-        </View>
-      </View>
-    </View>
-  )
+    )
 }
 
 
 const styles = EStyleSheet.create({
-    landingFooter:{
-        backgroundColor:'#141B2E',
-        paddingVertical:'1.5rem'
+    landingFooter: {
+        backgroundColor: '#141B2E',
+        paddingVertical: '1.5rem'
     },
-    footerLogo:{
-        alignItems:'center',
-        marginVertical:'2rem'
+    footerLogo: {
+        alignItems: 'center',
+        marginVertical: '2rem'
     },
-    footerLinkContainer:{
-        flexDirection:'row',
-        justifyContent:'space-evenly',
-        width:'100%',
-        marginBottom:'2rem'
-    },
-
-    linkLabel:{
-        color:'#fff'
+    footerLinkContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        width: '100%',
+        marginBottom: '2rem'
     },
 
-    socialMediaLinks:{
-        flexDirection:'row',
-        justifyContent:'space-evenly',
-        alignItems:'center',
-        borderTopWidth:1,
-        borderColor:'#989BA31A',
-        paddingTop:'2rem'
+    linkLabel: {
+        color: '#fff'
     },
 
-    copyrightTexts:{
-        color:'#fff',
-        fontSize:'0.9rem',
+    socialMediaLinks: {
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        borderTopWidth: 1,
+        borderColor: '#989BA31A',
+        paddingTop: '2rem'
+    },
+
+    copyrightTexts: {
+        color: '#fff',
+        fontSize: '0.9rem',
         color: '#B5B1B1',
-        marginVertical:'0.5rem'
+        marginVertical: '0.5rem'
     },
-    socialMediaIcons:{
-        flexDirection:'row',
-        justifyContent:'space-between',
-        gap:'2rem'
+    socialMediaIcons: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        gap: '2rem'
     },
 
-    icon:{
-        marginHorizontal:'1rem'
+    icon: {
+        marginHorizontal: '1rem'
+    },
+    contactUs: {
+        fontSize: '0.75rem',
+        color: '#EF2F55',
+        marginTop: '1rem',
+        fontFamily: 'graphik-medium',
+        textAlign: 'center'
     }
 })
 export default LandingFooter
