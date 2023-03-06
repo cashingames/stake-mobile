10c90b23a7ffcde94252e7fc3b55914d205efb96
Seyi_Coded <opadonuseyi01@gmail.com>
2023-03-06 15:06:32 +0100

    app.config.js

--- app.config.js
+++ app.config.js
@@ -1,12 +1,12 @@
 const env = process.env.APP_VARIANT;
 const isDevelopment = env === 'development' || env === 'local' || false;
-const version = "1.1.11"; //Update for every build and publish
+const version = "1.1.13"; //Update for every build and publish
 
 export default {
   name: getAppName(),
   slug: getSlug(),
   version: version,
-  runtimeVersion: "1.11", //All apps using the same runtime will get the published updates. Generally update for every new build
+  runtimeVersion: "1.13", //All apps using the same runtime will get the published updates. Generally update for every new build
   icon: "./assets/images/adaptive-icon2.png",
   jsEngine: "hermes",
   scheme: "gameark",
@@ -107,11 +107,11 @@ function getAndriodVersionCode() {
 
 function getAppName() {
   if (isDevelopment) {
-    return "Gameark Dev";
+    return "GameArk Dev";
   } else if (env === "preview") {
-    return "Gameark Test";
+    return "GameArk Test";
   } else {
-    return "Gameark";
+    return "GameArk";
   }
 }
 
