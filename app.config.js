fd62855b635b2156e25c6522309661f48b1f16d4
Seyi_Coded <opadonuseyi01@gmail.com>
2023-03-07 11:57:15 +0100

    icon change #version change

--- app.config.js
+++ app.config.js
@@ -1,12 +1,12 @@
 const env = process.env.APP_VARIANT;
 const isDevelopment = env === 'development' || env === 'local' || false;
-const version = "1.1.13"; //Update for every build and publish
+const version = "1.1.14"; //Update for every build and publish
 
 export default {
   name: getAppName(),
   slug: getSlug(),
   version: version,
-  runtimeVersion: "1.13", //All apps using the same runtime will get the published updates. Generally update for every new build
+  runtimeVersion: "1.14", //All apps using the same runtime will get the published updates. Generally update for every new build
   icon: "./assets/images/adaptive-icon2.png",
   jsEngine: "hermes",
   scheme: "gameark",
