2232ab64c11a55804212a242023642405dc819a4
ererobe95 <danielererobe@gmail.com>
2023-04-04 13:31:40 +0100

    sending a one-time push notification message to users when they install the app

--- src/shared/Notification.js
+++ src/shared/Notification.js
@@ -1,3 +1,4 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as Notifications from 'expo-notifications';
 
 export const triggerNotifierForReferral = ()=>{
@@ -8,4 +9,20 @@ export const triggerNotifierForReferral = ()=>{
         })
     }, 7000)
     
+}
+
+export const triggerNotificationForAppInstallation = async () => {
+    let token = await AsyncStorage.getItem('expoPushToken');
+    if (!token) {
+      token = await Notifications.getExpoPushTokenAsync();
+      await AsyncStorage.setItem('expoPushToken', JSON.stringify(token));
+     Notifications.scheduleNotificationAsync({
+        content: {
+          title: 'Welcome to Gameark',
+          body: "Welcome to GameArk, where the adventure never ends! Whether you're a seasoned player or new to the gaming world, our game has something for everyone. With exciting quests and endless challenges, get ready to dive into a world of fun and excitement. Join our community today and let's begin the journey together!🎮🌟",
+          importance: 'high',
+        },
+        trigger: null,
+      });
+    }
 }
