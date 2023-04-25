b8994d570e1263954b38a56495db4e3d582bd773
Seyi_Coded <opadonuseyi01@gmail.com>
2023-04-25 12:39:24 +0100

    ios fix

--- app.config.js
+++ app.config.js
@@ -1,13 +1,13 @@
 const env = process.env.APP_VARIANT;
 const isDevelopment = env === 'development' || env === 'local' || false;
 
-const version = "1.1.25"; //Update for every build and publish
+const version = "1.1.26"; //Update for every build and publish
 
 export default {
   name: getAppName(),
   slug: getSlug(),
   version: version,
-  runtimeVersion: "1.25", //All apps using the same runtime will get the published updates. Generally update for every new build
+  runtimeVersion: "1.26", //All apps using the same runtime will get the published updates. Generally update for every new build
   icon: "./assets/images/adaptive-icon2.png",
   jsEngine: "hermes",
   scheme: "gameark",
