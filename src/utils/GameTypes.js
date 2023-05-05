6ca50451d31d14344a77088f608a27badbc2c80a
ererobe95 <danielererobe@gmail.com>
2023-05-05 15:25:09 +0100

    adding padding rignt to scrollview on GameScreen component and removing horizontal scrollbar indicator

---
+++ src/utils/GameTypes.js
@@ -0,0 +1,31 @@
+
+ export const gamesType = [
+    {
+        id: 1,
+        gameName: 'Trivia Hub',
+        backgroundImage: require('../../assets/images/quiz-background.png'),
+        gameImage: require('../../assets/images/word-trivia2.png'),
+        unlocked: true
+    },
+    {
+        id: 2,
+        gameName: 'picture trivia',
+        backgroundImage: require('../../assets/images/picture-trivia.png'),
+        gameImage: require('../../assets/images/picture-trivia-icon.png'),
+        unlocked: false
+    },
+    {
+        id: 3,
+        gameName: 'word swap',
+        backgroundImage: require('../../assets/images/word-snap.png'),
+        gameImage: require('../../assets/images/wordswap-icon.png'),
+        unlocked: false
+    },
+    {
+        id: 4,
+        gameName: 'picture jumbo',
+        backgroundImage: require('../../assets/images/picture-jumbo.png'),
+        gameImage: require('../../assets/images/picturejumbo-icon.png'),
+        unlocked: false
+    },
+]
